8d55660ede4f5b0ca8c517e1a91ec2abada72cdc
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-07-28 01:50:42 +0300

    started to make login page

--- PENKOFF/Controllers/AccountController.cs
+++ PENKOFF/Controllers/AccountController.cs
@@ -5,8 +5,13 @@ namespace PENKOFF.Controllers;
 public class AccountController : Controller
 {
     // GET
-    public IActionResult Account()
+    public async Task<IActionResult> Account()
     {
+        if (HttpContext.Session.GetInt32("Id") == null)
+        {
+            return View("~/Views/Account/Login.cshtml");
+        }
+        
         return View();
     }
 }
